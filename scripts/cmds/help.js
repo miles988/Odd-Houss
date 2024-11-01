ddbe9dff0c2b59797bf5f5555abecfd99f82b752
miles988 <odiamuslonon@gmail.com>
2024-11-01 14:40:45 +0100

    Add files via upload

---
+++ scripts/cmds/help.js
@@ -0,0 +1,133 @@
+ const fs = require("fs-extra");
+const axios = require("axios");
+const path = require("path");
+const { getPrefix } = global.utils;
+const { commands, aliases } = global.GoatBot;
+const doNotDelete = "[ 🍻 | Odiamus AI ]";
+
+module.exports = {
+  config: {
+    name: "help",
+    version: "2.4",
+    author: "Odiamus", // it's owner odiamus 
+    countDown: 0,
+    role: 0,
+    shortDescription: {
+      en: "View command usage",
+    },
+    longDescription: {
+      en: "View command usage and list all commands directly",
+    },
+    category: "info",
+    guide: {
+      en: "{pn} / help cmdName ",
+    },
+    priority: 1,
+  },
+
+  onStart: async function ({ message, args, event, threadsData, role }) {
+  const { threadID } = event;
+  const threadData = await threadsData.get(threadID);
+  const prefix = getPrefix(threadID);
+
+  if (args.length === 0) {
+      const categories = {};
+      let msg = "";
+
+      msg += `╔══════════════╗\n    Odiamus Lonon 🍻🍹 \n╚══════════════╝`;
+
+      for (const [name, value] of commands) {
+          if (value.config.role > 1 && role < value.config.role) continue;
+
+          const category = value.config.category || "Uncategorized";
+          categories[category] = categories[category] || { commands: [] };
+          categories[category].commands.push(name);
+      }
+8
+      Object.keys(categories).forEach(category => {
+          if (category !== "info") {
+              msg += `\n╭────────────⭓\n│『 ${category.toUpperCase()} 』`;
+
+              const names = categories[category].commands.sort();
+              for (let i = 0; i < names.length; i += 1) {
+                  const cmds = names.slice(i, i + 1).map(item => `│✧${item}`);
+                  msg += `\n${cmds.join(" ".repeat(Math.max(0, 5 - cmds.join("").length)))}`;
+              }
+
+              msg += `\n╰────────⭓`;
+          }
+      });
+
+      const totalCommands = commands.size;
+      msg += `\nCurrently, i have  ${totalCommands} commands that can be used.Soon more commands will be added\n`;
+      msg += `Type ${prefix} help command Name to view the details of that command\n`;
+      msg += `Øđiløn GaotBot V2 (•̀ᴗ•́)و`;
+
+
+      const helpListImages = [
+"https://i.imgur.com/dOAZf6R.jpeg",
+"https://i.imgur.com/AIz8ASV.jpeg",
+"https://i.imgur.com/KhnL8U8.jpeg", 
+ "https://i.imgur.com/5IRfh3C.jpeg",
+ "https://i.imgur.com/EHg0xW0.jpeg",   "https://i.imgur.com/v8HFO5c.jpeg",  "https://i.imgur.com/auavNDT.jpeg"
+];
+
+
+      const helpListImage = helpListImages[Math.floor(Math.random() * helpListImages.length)];
+
+
+      await message.reply({
+          body: msg,
+          attachment: await global.utils.getStreamFromURL(helpListImage)
+      });
+  } else {
+      const commandName = args[0].toLowerCase();
+      const command = commands.get(commandName) || commands.get(aliases.get(commandName));
+
+      if (!command) {
+        await message.reply(`Command "${commandName}" not found.`);
+      } else {
+        const configCommand = command.config;
+        const roleText = roleTextToString(configCommand.role);
+        const author = configCommand.author || "Unknown";
+
+        const longDescription = configCommand.longDescription ? configCommand.longDescription.en || "No description" : "No description";
+
+        const guideBody = configCommand.guide?.en || "No guide available.";
+        const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
+
+        const response = `╭── NAME ────⭓
+  │ ${configCommand.name}
+  ├── INFO
+  │ Description: ${longDescription}
+  │ Other names: ${configCommand.aliases ? configCommand.aliases.join(", ") : "Do not have"}
+  │ Other names in your group: Do not have
+  │ Version: ${configCommand.version || "1.0"}
+  │ Role: ${roleText}
+  │ Time per command: ${configCommand.countDown || 1}s
+  │ Author: ${author}
+  ├── Usage
+  │ ${usage}
+  ├── Notes
+  │ The content inside <❄️❄️> can be changed
+  │ The content inside [a|b|c] is a or b or c
+  ╰━━━━━━━❖`;
+
+        await message.reply(response);
+      }
+    }
+  },
+};
+
+function roleTextToString(roleText) {
+  switch (roleText) {
+    case 0:
+      return "0 (All users)";
+    case 1:
+      return "1 (Group administrators)";
+    case 2:
+      return "2 (Admin bot)";
+    default:
+      return "Unknown role";
+  }
+  }
